82e42dc0588791b301696dd03111a224c1554a02
YogeshYogi-creator <bl.yogesh11@gmail.com>
2021-04-21 09:43:02 +0530

    hack

---
+++ src/MessagesChart.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Chart from "react-google-charts"
+
+const MessagesChart = (props) => {
+    const {userMessagesLength} = props
+    
+    const chartData = [
+      ['User Name', 'Messages Count']
+    ]
+
+    // for(const key in data){
+    //   chartData.push([key, data[key]])
+    // }
+
+    for(const message of userMessagesLength){
+        chartData.push([...message] )
+    }
+
+    return (
+        <div className="col-md-6" style={{margin : '0px'}}>
+        <Chart
+        width={'700px'}
+        height={'500px'}
+        chartType="PieChart"
+        loader={<div>Loading Chart</div>}
+        data={chartData}
+        options={{
+          title: 'Messages Count',
+        }}
+        rootProps={{ 'data-testid': '1' }}
+      />
+        </div>
+    )
+}
+
+export default MessagesChart
